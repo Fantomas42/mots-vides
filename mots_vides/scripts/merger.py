d3621dadd901d76c21c14adfae93365619dd1773
Fantomas42 <fantomas42@gmail.com>
2015-01-24 16:45:50 +0100

    Write the merger script, with the expected API

--- mots_vides/scripts/merger.py
+++ mots_vides/scripts/merger.py
@@ -1,8 +1,13 @@
 """
 Merger command line script for building collections of stop words
 """
+import os
 import sys
 from argparse import ArgumentParser
+
+from mots_vides.constants import DATA_DIRECTORY
+from mots_vides.stop_words import StopWord
+from mots_vides.stop_words import StopWordFactory
 
 
 def cmdline(argv=sys.argv[1:]):
@@ -13,4 +18,17 @@ def cmdline(argv=sys.argv[1:]):
     parser.add_argument('sources', metavar='FILE', nargs='+',
                         help='Source files to parse')
     options = parser.parse_args(argv)
-    import pdb; pdb.set_trace()
+
+    stop_words = StopWordFactory(
+        fail_safe=True).get_stop_words(options.language)
+
+    for source in options.sources:
+        with open(source) as source_fd:
+            collection = filter(str.strip, source_fd.readlines())
+        stop_words += StopWord(options.language, collection)
+
+    data_file = os.path.join(DATA_DIRECTORY, '%s.txt' % options.language)
+    collection = stop_words.collection.sort()
+    with open(data_file, 'w+') as data_fd:
+        data_fd.truncate()
+        data_fd.write('\n'.join(collection))
