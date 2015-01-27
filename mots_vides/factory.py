ca4556d1db430b4d80b97047ef24a7d527d589d1
Fantomas42 <fantomas42@gmail.com>
2015-01-27 22:56:51 +0100

    Optimize the cache when querying a collection

--- mots_vides/factory.py
+++ mots_vides/factory.py
@@ -31,15 +31,15 @@ class StopWordFactory(object):
         """
         collection = self.LOADED_LANGUAGES_CACHE.get(language)
 
-        if not collection:
+        if collection is None:
             try:
                 collection = self._get_stop_words(language)
             except StopWordError as error:
                 if not fail_safe:
                     raise error
                 collection = []
+            self.LOADED_LANGUAGES_CACHE[language] = collection
 
-        self.LOADED_LANGUAGES_CACHE[language] = collection
         stop_words = StopWord(language, collection)
         return stop_words
 
