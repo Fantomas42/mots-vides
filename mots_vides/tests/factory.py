27d7e6240d92944c93cbac4ee4be11d5eeecf8ec
Fantomas42 <fantomas42@gmail.com>
2015-01-27 23:34:07 +0100

    Update the tests of the factory

--- mots_vides/tests/factory.py
+++ mots_vides/tests/factory.py
@@ -7,45 +7,32 @@ from unittest import TestCase
 
 from mots_vides.stop_words import StopWord
 from mots_vides.factory import StopWordFactory
+from mots_vides.exceptions import StopWordError
 
 
 class StopWordFactoryTestCase(TestCase):
 
-    factory = StopWordFactory(os.path.join(
-        os.path.dirname(
-            os.path.abspath(__file__)), 'datas/'
-        )
-    )
-    language = 'foo'
-    expection_file_collection = set(['bla', 'foo', 'bar',
-                                     'woot', 'doublewoot'])
-    expected_filename = 'foo.txt'
-
-    def _flush_cash(self):
-        pass
+    def setUp(self):
+        self.data_directory = os.path.join(
+            os.path.dirname(
+                os.path.abspath(__file__)),
+            'datas/')
+        self.factory = StopWordFactory(self.data_directory)
 
     def test_get_stopwords(self):
-        sw = self.factory.get_stop_words(self.language)
+        sw = self.factory.get_stop_words('foo')
         self.assertTrue(isinstance(sw, StopWord))
-        self.assertEqual(sw.collection, self.expection_file_collection)
-        self._flush_cash()
+        self.assertEqual(list(sw.collection),
+                         ['bla', 'foo', 'bar',
+                          'woot', 'doublewoot'])
 
         sw = self.factory.get_stop_words('blabla', fail_safe=True)
         self.assertEqual(len(sw), 0)
-        self._flush_cash()
 
         self.factory = StopWordFactory()
-        self.assertRaises(
-            Exception,
-            lambda: self.factory.get_stop_words,
-            'blabla'
-        )
+        self.assertRaises(StopWordError, self.factory.get_stop_words, 'blabla')
 
     def test_get_collection_filename(self):
-        self.assertRaises(
-            Exception,
-            lambda: self.factory.get_collection_filename,
-            'jkhj'
-        )
         filename = self.factory.get_collection_filename('foo')
-        self.assertEqual(filename, self.expected_filename)
+        self.assertTrue(filename.endswith('foo.txt'))
+        self.assertTrue(filename.startswith(self.data_directory))
