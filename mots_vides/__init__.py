6f9283fe9a80ca0dd2480f0a5aa73268e7098f98
Fantomas42 <fantomas42@gmail.com>
2015-02-03 22:33:57 +0100

    Add a shortcut

--- mots_vides/__init__.py
+++ mots_vides/__init__.py
@@ -4,4 +4,15 @@ Mots-vides
 from mots_vides.stop_words import StopWord
 from mots_vides.factory import StopWordFactory
 
-__all__ = ['StopWord', 'StopWordFactory']
+__all__ = ['StopWord', 'StopWordFactory',
+           'default_factory', 'stop_words']
+
+default_factory = StopWordFactory()
+
+def stop_words(language, fail_safe=True):
+    """
+    Shortcut for getting stop words
+    without initializing a factory.
+    """
+    return default_factory.get_stop_words(
+        language, fail_safe=fail_safe)
