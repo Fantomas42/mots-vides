276591199e2c40d49ee5ace3a83de8432f66584b
Fantomas42 <fantomas42@gmail.com>
2015-02-04 21:37:22 +0100

    Add korean language

--- rebuild-collections.sh
+++ rebuild-collections.sh
@@ -5,6 +5,7 @@ path=${1-mots_vides/datas/}
 
 for lang in $(ls $path)
 do
-  lang=$(echo $lang | cut -f1 -d.)
-  ./bin/merge-stop-words $lang $path$lang.txt
+  file=$(echo $lang | cut -f1 -d.)
+  lang=$(echo $file | cut -f2 -d-)
+  ./bin/merge-stop-words $lang $path$file.txt
 done
