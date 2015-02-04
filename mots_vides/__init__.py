e876473de15e356aa10613ab16802ee634ebe29c
Fantomas42 <fantomas42@gmail.com>
2015-02-04 12:26:30 +0100

    Import StopWordError

--- mots_vides/__init__.py
+++ mots_vides/__init__.py
@@ -3,8 +3,9 @@ Mots-vides
 """
 from mots_vides.stop_words import StopWord
 from mots_vides.factory import StopWordFactory
+from mots_vides.exceptions import StopWordError
 
-__all__ = ['StopWord', 'StopWordFactory',
+__all__ = ['StopWord', 'StopWordFactory', 'StopWordError',
            'default_factory', 'stop_words']
 
 default_factory = StopWordFactory()
